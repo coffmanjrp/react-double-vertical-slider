9ddea0c610cd99dcf939c53003e6602e57220d76
coffmanjrp <coffmanjrp@gmail.com>
2022-01-28 16:33:01 -0800

    Update README

--- src/utils/slides.js
+++ src/utils/slides.js
@@ -3,7 +3,7 @@ const slides = [
     id: 1,
     color: '#fd3555',
     image:
-      'https://images.unsplash.com/photo-1510942201312-84e7962f6dbb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=da4ca7a78004349f1b63f257e50e4360&auto=format&fit=crop&w=1050&q=80',
+      'https://images.unsplash.com/photo-1596985122625-faf96c53e0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
     heading: 'Nature flower',
     lead: 'All in pink',
   },
@@ -11,25 +11,25 @@ const slides = [
     id: 2,
     color: '#2a86ba',
     image:
-      'https://images.unsplash.com/photo-1486899430790-61dbf6f6d98b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8ecdee5d1b3ed78ff16053b0227874a2&auto=format&fit=crop&w=1002&q=80',
+      'https://images.unsplash.com/photo-1517758478390-c89333af4642?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
     heading: 'Blue Sky',
-    lead: "with it's mountains",
+    lead: 'with white clouds',
   },
   {
     id: 3,
     color: '#252e33',
     image:
-      'https://images.unsplash.com/photo-1519981593452-666cf05569a9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=90ed8055f06493290dad8da9584a13f7&auto=format&fit=crop&w=715&q=80',
-    heading: 'Lonely castle',
-    lead: 'in the wilderness',
+      'https://images.unsplash.com/photo-1480018381142-5ab7b36d3098?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    heading: 'Perfect Night',
+    lead: 'Silent and clear',
   },
   {
     id: 4,
     color: '#ffb866',
     image:
-      'https://images.unsplash.com/photo-1508768787810-6adc1f613514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e27f6661df21ed17ab5355b28af8df4e&auto=format&fit=crop&w=1350&q=80',
-    heading: 'lying eagle',
-    lead: 'in the sunset',
+      'https://images.unsplash.com/photo-1542159919831-40fb0656b45a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    heading: 'The Sunset',
+    lead: 'End of the day',
   },
 ];
 
